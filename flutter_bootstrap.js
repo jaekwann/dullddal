e233c8dde8ec8a2097e7aba6087926be95db303d
jaekwan <wprhks59@naver.com>
2026-02-11 13:44:58 +0900

    Deploy web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "993897009"
+    serviceWorkerVersion: "1288568389"
   }
 });
